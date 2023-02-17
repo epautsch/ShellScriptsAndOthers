a2a7e8756bfdb685def377e4a68029355785d9d8
= <epautsch@luc.edu>
2023-02-16 23:07:00 -0600

    change 1

--- commits2graph.py
+++ commits2graph.py
@@ -7,7 +7,9 @@ def search_for_json_files(search_directory):
     # search for all json files in the directory
     json_files = glob.glob(os.path.join(search_directory, '*.json'))
 
-    # print the names of each file to the console
+    # store the name of each json file in a list without the full path
+    json_files = [os.path.basename(json_file) for json_file in json_files]
+
     for file in json_files:
         print(file)
 
